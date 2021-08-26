f6ca48c40f0479291588a0da487fae577cc2d8ce
Mavericks(Bishal) <mavericksthinker@gmail.com>
2021-08-26 14:55:35 -0700

    Implement intersection observer for lazy loading of background image

--- src/components/Movie.vue
+++ src/components/Movie.vue
@@ -1,7 +1,9 @@
 <template>
   <a class="movie-link" :href="movie.pageUrl" target="_blank">
-  <div class="movie-container flex align-items__flex-end flex-col justify-content__flex-end"
-       :style="{ backgroundImage: `url(${movie.posterUrl})` }"
+  <div ref="movieContainer"
+       :id="`movie-container-${movie.id}`"
+       class="movie-container flex align-items__flex-end flex-col justify-content__flex-end"
+       :data-src="movie.posterUrl"
   >
     <div class="movie-card">
       <div class="movie-title">{{ movie.title }}</div>
@@ -26,12 +28,45 @@ export default defineComponent({
   props: {
     movie: {},
   },
+  data() {
+    return {
+      movieContainer: null,
+      movieContainerObserver: null,
+      movieContainerObserverOption: {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.5,
+      },
+    };
+  },
+  mounted() {
+    this.registerMovieContainerObserver();
+  },
+  methods: {
+    handleMovieContainerObserver(entries) {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          this.movieContainer.style.backgroundImage = `url(${this.movie.posterUrl})`;
+          this.movieContainerObserver.disconnect();
+        }
+      });
+    },
+    registerMovieContainerObserver() {
+      this.movieContainer = document.querySelector(`#movie-container-${this.movie.id}`);
+      if (this.movieContainer) {
+        this.movieContainerObserver = new IntersectionObserver(
+          this.handleMovieContainerObserver,
+          this.movieContainerObserverOption,
+        );
+        this.movieContainerObserver.observe(this.movieContainer);
+      }
+    },
+  },
   computed: {
     getMovieRating() {
       return `${this.movie.rating || 0}`;
     },
     getMovieStarring() {
-      console.log(this.movie);
       return `Starring: ${this.movie.actorIds.join(', ')}`;
     },
     getMoviePlot() {
